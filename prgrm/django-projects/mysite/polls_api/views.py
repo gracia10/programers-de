47fd585526cb5cc35a4eb210d415642d27a73f6e
MACLSH <gracia10@daum.net>
2023-04-26 22:26:40 +0900

    Add REST API using api_view decorator

--- prgrm/django-projects/mysite/polls_api/views.py
+++ prgrm/django-projects/mysite/polls_api/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from polls.models import Question
+from polls_api.serializers import QuestionSerializer
+
+
+@api_view(['GET', 'POST'])
+def question_list(request):
+    if request.method == 'GET':
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def question_detail(request, id):
+    questions = get_object_or_404(pk=id)
+
+    if request.method == 'GET':
+        serializer = QuestionSerializer(questions)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = QuestionSerializer(questions)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        questions.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
