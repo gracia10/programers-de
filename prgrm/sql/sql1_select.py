a6bf5855808de98e2722b1ae38e5329fca83655e
MACLSH <gracia10@daum.net>
2023-05-12 21:16:33 +0900

    Fix : separate function

--- prgrm/sql/sql1_select.py
+++ prgrm/sql/sql1_select.py
@@ -3,29 +3,44 @@ import psycopg2
 
 from dotenv import load_dotenv
 
+
 # .env 파일에서 환경 변수 로드
-load_dotenv()
+def _get_db_info() -> dict:
+    load_dotenv()
 
-host = os.getenv("DATABASE_HOST")
-dbname = os.getenv("DATABASE_NAME")
-user = os.getenv("DATABASE_USER")
-password = os.getenv("DATABASE_PASSWORD")
-port = os.getenv("DATABASE_PORT")
+    dbinfo = {'host': os.getenv("DATABASE_HOST"), 'dbname': os.getenv("DATABASE_NAME"),
+              'user': os.getenv("DATABASE_USER"), 'password': os.getenv("DATABASE_PASSWORD"),
+              'port': os.getenv("DATABASE_PORT")}
+    return dbinfo
 
-conn = psycopg2.connect(
-    host=host,
-    dbname=dbname,
-    user=user,
-    password=password,
-    port=port
-)
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM raw_data.user_session_channel limit 1")
-records = cursor.fetchone()
+def _get_Redshift_connection(autocommit, host, dbname, user, password, port):
+    conn = psycopg2.connect(
+        host=host,
+        dbname=dbname,
+        user=user,
+        password=password,
+        port=port
+    )
+    conn.set_session(autocommit=autocommit)
+    return conn
 
-for record in records:
-    print(record)
 
-cursor.close()
-conn.close()
+def main():
+    dbinfo = _get_db_info()
+    conn = _get_Redshift_connection(autocommit=True, **dbinfo)
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM raw_data.user_session_channel limit 10")
+
+    records = cursor.fetchall()
+
+    for record in records:
+        print(record)
+
+    cursor.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
