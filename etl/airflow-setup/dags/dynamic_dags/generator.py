d20488ad19768a3b56fc6a8468ed89009452175e
MACLSH <gracia10@daum.net>
2023-06-21 21:09:54 +0900

    feat : Dynamic Dag
    DAG를 동적으로 생성한다

---
+++ etl/airflow-setup/dags/dynamic_dags/generator.py
@@ -0,0 +1,17 @@
+import os
+
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+# 템플릿 객체를 로드하는 로더와,
+file_dir = os.path.dirname(os.path.abspath(__file__))
+env = Environment(loader=FileSystemLoader(file_dir))
+template = env.get_template('templated_dag.jinja2')
+
+file_dir += '/config'
+for f in os.listdir(file_dir):
+    if f.endswith(".yml"):
+        with open(f"{file_dir}/{f}", "r") as cf:
+            config = yaml.safe_load(cf)
+            with open(f"dags/get_price_{config['dag_id']}.py", "w") as f:
+                f.write(template.render(config))
